a9825947eec28335e0889aad783f1d8452bedfc8
xugaoyi <894072666@qq.com>
2019-11-05 18:45:16 +0800

    init

---
+++ TempConvert.py
@@ -0,0 +1,11 @@
+# 摄氏度与华氏度的转换
+TempStr = input("请输入带有符号的温度值：")
+print(TempStr[0:1])
+if TempStr[-1] in ['F','f']:
+    C = (eval(TempStr[0:-1]) - 32)/1.8
+    print("转换后的温度是{:.2f}C".format(C))
+elif TempStr[-1] in ['C','c']:
+    F = 1.8 * eval(TempStr[0:-1]) + 32
+    print("转换后的温度是{:.2f}F".format(F))
+else:
+    print("输入格式错误")
---
+++ test.py
@@ -0,0 +1,9 @@
+# 画五角星
+from turtle import *
+color('yellow','red')
+begin_fill()
+for i in range(5):
+    fd(180)
+    rt(144)
+end_fill()
+done()
